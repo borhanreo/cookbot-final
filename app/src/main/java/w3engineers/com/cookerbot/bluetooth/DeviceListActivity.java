45b349a1a7a7b58d3a6938c77ec475d4cd720de1
borhan <borhan@w3engineers.com>
2017-03-03 16:25:25 +0600

    Start

--- app/src/main/java/w3engineers/com/cookerbot/bluetooth/DeviceListActivity.java
+++ app/src/main/java/w3engineers/com/cookerbot/bluetooth/DeviceListActivity.java
@@ -24,9 +24,9 @@ import w3engineers.com.cookerbot.activity.IoTActivity;
 public class DeviceListActivity extends Activity {
     private static final String TAG = "DeviceListActivity";
     private static final boolean D = true;
-    Button tlbutton;
+
     TextView textView1;
-    
+
     // EXTRA string to send on to mainactivity
     public static String EXTRA_DEVICE_ADDRESS = "device_address";
 
@@ -41,51 +41,48 @@ public class DeviceListActivity extends Activity {
     }
 
     @Override
-    public void onResume() 
-    {
-    	super.onResume();
-    	checkBTState();
-    	textView1 = (TextView) findViewById(R.id.connecting);
-    	textView1.setTextSize(40);
-    	textView1.setText(" ");
-    	mPairedDevicesArrayAdapter = new ArrayAdapter<String>(this, R.layout.device_name);
-    	ListView pairedListView = (ListView) findViewById(R.id.paired_devices);
-    	pairedListView.setAdapter(mPairedDevicesArrayAdapter);
-    	pairedListView.setOnItemClickListener(mDeviceClickListener);
-    	mBtAdapter = BluetoothAdapter.getDefaultAdapter();
+    public void onResume() {
+        super.onResume();
+        checkBTState();
+        textView1 = (TextView) findViewById(R.id.connecting);
+        textView1.setTextSize(40);
+        textView1.setText(" ");
+        mPairedDevicesArrayAdapter = new ArrayAdapter<String>(this, R.layout.device_name);
+        ListView pairedListView = (ListView) findViewById(R.id.paired_devices);
+        pairedListView.setAdapter(mPairedDevicesArrayAdapter);
+        pairedListView.setOnItemClickListener(mDeviceClickListener);
+        mBtAdapter = BluetoothAdapter.getDefaultAdapter();
 
-    	// Get a set of currently paired devices and append to 'pairedDevices'
-    	Set<BluetoothDevice> pairedDevices = mBtAdapter.getBondedDevices();
+        // Get a set of currently paired devices and append to 'pairedDevices'
+        Set<BluetoothDevice> pairedDevices = mBtAdapter.getBondedDevices();
 
-    	// Add previosuly paired devices to the array
-    	if (pairedDevices.size() > 0) {
-    		findViewById(R.id.title_paired_devices).setVisibility(View.VISIBLE);//make id viewable
-    		for (BluetoothDevice device : pairedDevices) {
-    			mPairedDevicesArrayAdapter.add(device.getName() + "\n" + device.getAddress());
-    		}
-    	} else {
-    		String noDevices = getResources().getText(R.string.none_paired).toString();
-    		mPairedDevicesArrayAdapter.add(noDevices);
-    	}
-  }
+        // Add previosuly paired devices to the array
+        if (pairedDevices.size() > 0) {
+            findViewById(R.id.title_paired_devices).setVisibility(View.VISIBLE);//make id viewable
+            for (BluetoothDevice device : pairedDevices) {
+                mPairedDevicesArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+            }
+        } else {
+            String noDevices = getResources().getText(R.string.none_paired).toString();
+            mPairedDevicesArrayAdapter.add(noDevices);
+        }
+    }
 
     // Set up on-click listener for the list (nicked this - unsure)
     private OnItemClickListener mDeviceClickListener = new OnItemClickListener() {
         public void onItemClick(AdapterView<?> av, View v, int arg2, long arg3) {
-            try
-            {
+            try {
                 textView1.setText("Connecting...");
                 // Get the device MAC address, which is the last 17 chars in the View
                 String info = ((TextView) v).getText().toString();
                 String address = info.substring(info.length() - 17);
-
+                Log.d(TAG, " borhan " + address);
                 // Make an intent to start next activity while taking an extra which is the MAC address.
                 Intent i = new Intent(DeviceListActivity.this, IoTActivity.class);
                 i.putExtra(EXTRA_DEVICE_ADDRESS, address);
                 startActivity(i);
-            }catch (Exception ex)
-            {
-                Log.d("borhan","borhan "+ex.toString());
+            } catch (Exception ex) {
+                Log.d("borhan", "borhan " + ex.toString());
             }
 
         }
@@ -93,18 +90,18 @@ public class DeviceListActivity extends Activity {
 
     private void checkBTState() {
         // Check device has Bluetooth and that it is turned on
-    	 mBtAdapter=BluetoothAdapter.getDefaultAdapter(); // CHECK THIS OUT THAT IT WORKS!!!
-        if(mBtAdapter==null) { 
-        	Toast.makeText(getBaseContext(), "Device does not support Bluetooth", Toast.LENGTH_SHORT).show();
+        mBtAdapter = BluetoothAdapter.getDefaultAdapter(); // CHECK THIS OUT THAT IT WORKS!!!
+        if (mBtAdapter == null) {
+            Toast.makeText(getBaseContext(), "Device does not support Bluetooth", Toast.LENGTH_SHORT).show();
         } else {
-          if (mBtAdapter.isEnabled()) {
-            Log.d(TAG, "...Bluetooth ON...");
-          } else {
-            //Prompt user to turn on Bluetooth
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, 1);
- 
+            if (mBtAdapter.isEnabled()) {
+                Log.d(TAG, "...Bluetooth ON...");
+            } else {
+                //Prompt user to turn on Bluetooth
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, 1);
+
             }
-          }
         }
+    }
 }
